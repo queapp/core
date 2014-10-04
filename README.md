6cfb4791de63890ea1826ed1bf0faefac942d7c1
1egoman <rsg1egoman@gmail.com>
2014-10-04 07:55:26 -0400

    created documentation and working client stuffs

---
+++ README.md
@@ -0,0 +1,92 @@
+Que's IoT Controller
+===
+
+### What is this?
+This app is an fancy [IoT](http://en.wikipedia.org/wiki/Internet_of_Things) controller.
+However, what makes this app different is it's easy to host yourself, and it is a relatively easy
+to develop your own things. Every other platform that I've seen has a dashboard of some sort that
+you can configure stuff, and most likely get nearly what you want. It works reasonably well, but
+I, like many people, want to be able to control every aspect of each thing. That is the niche this
+app plans to fill.
+
+### Ok, that sounds cool. How do I make this "thing" you speak of?
+Well, it's pretty simple if you're familiar in Javascript and Node. A simple thing could look like
+this:
+
+index.js
+```javascript
+  var thing = require("iot-thing"); // make sure to add this to your package.json!
+
+  // this is your app's id, set it to null and one will be auto-assigned to you
+  // the id is available below as thing.id, so store it somewhere for next run and pass it in here
+  id = null  
+
+  // create the thing
+  // specify the hostname and port of the server, the id, and
+  // the structure object. This contains all the default settings for the thing.
+  new thing("127.0.0.1", 8000, id, {
+    name: "Example Thing",
+    desc: "Prooves that stuff works",
+    data: {
+      message: {
+        value: "Hello World"
+      },
+      showMessage: {
+        value: false,
+        label: "Show message in terminal"
+      }
+    }
+  }, function(thing) {
+    // get the thing id, and print it out
+    console.log("Thing ID is", thing.id);
+
+    // did the user set showMessage to true?
+    thing.data.pull("showMessage", function(val) {
+      if (val == true) {
+        // set it to false
+        thing.data.push("showMessage", false);
+
+        // show the message in the console
+        thing.data.pull("message", function(val) {
+          console.log("Output:", val);
+        });
+      }
+    });
+  });
+
+```
+
+To run it:
+  - Terminal window 1: `node index.js` (inside the root of this repository)
+  - Terminal window 2: `node index.js` (inside the client thing you just made)
+
+*NOTE: If you want to see what is happening in a nice, fancy web ui, I'd also recommend in a 3rd window
+(I know, starting to get a little crazy with the terminal windows) running
+`cd public; python -m SimpleHTTPServer 8001` and go to 127.0.0.1:8001 in a web browser before running any
+of the above commands*
+
+### License
+Well, this is an interesting topic, actually. All the code you see right now is under the MIT License, but
+I will be make some closed source thing's that people could buy from me later on. In short, this repository
+under the MIT but not every thing has to be.
+
+The MIT License (MIT)
+Copyright (c) 2014 Ryan Gaus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- client/index.js
+++ client/index.js
@@ -1,201 +1,47 @@
-var request = require("request");
-// var gpio = require("pi-gpio");
-var strftime = require("strftime");
-
-var thing = function(host, port, id, structure, callback) {
-
-  var root = this;
-
-  // does the specific id specified exist on the server backend?
-  this.doesIdExist = function(id, callback) {
-    request.get({
-      url: "http://" + host + ":" + port + "/things/" + id + "/data",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }, function(error, response, body) {
-      body = JSON.parse(body);
-      callback( body.status != "NOHIT" );
-    });
-  }
+// iot-thing example code
+// by Ryan Gaus (1egoman)
+// published under the MIT License
+// you must host a iot-server locally on port :8000 for this to work
 
 
-  this.data = {
+// the library
+var thing = require("iot-thing");
 
-    push: function(property, val, done) {
-      var d = {}
-      d[property] = {
-        value: val
-      };
+// this is your app's id, set it to null and one will be auto-assigned to you
+// the id is available below as thing.id, so store it somewhere for next time and pass it in here
+id = 6
 
-      this.cache = {};
-
-      request.put(
-        {
-          url: "http://" + host + ":" + port + "/things/" + root.id + "/data",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(d)
-        }, function(error, response, body) {
-          done && done(body);
-        }
-      );
-    },
-
-
-    pull: function(property, done) {
-      request.get(
-        {
-          url: "http://" + host + ":" + port + "/things/" + root.id + "/data",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }, function(error, response, body) {
-          body = body && JSON.parse(body);
-          root.cache = body;
-          done && done(body && body[property] || undefined);
-        }
-      );
-    }
-
-  }
-
-  // did user give a numerical id?
-  this.doesIdExist(id, function(doesIt) {
-    if (doesIt == false || typeof id != "number") {
-      request.post(
-        {
-          url: "http://" + host + ":" + port + "/things/add",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(structure)
-        }, function(error, response, body) {
-          body = JSON.parse(body);
-          root.id = body.id;
-
-          callback(root);
-        }
-      );
-    } else {
-      root.id = id;
-      callback(root);
-    }
-  });
-
-}
-
-
-id = 5;
-
-// t = new thing("que-app-backend.herokuapp.com", 80, id, {
-t = new thing("127.0.0.1", 8000, id, {
-  name: "Hydroponic Garden",
-  desc: "A garden that grows me stuff",
-  tags: ["garden", "hydroponics"],
-  image: "http://panamashippingcontainerhouse.com/wp-content/uploads/2012/07/Hydroponics-650x487.jpg",
+// create the thing
+// specify the hostname and port of the server, the id, and
+// the structure object. This contains all the default settings for the thing.
+new thing("127.0.0.1", 8000, id, {
+  name: "Example Thing",
+  desc: "Prooves that stuff works",
   data: {
-    wateringTime: {
-      value: "11:00:00"
+    message: {
+      value: "Hello World"
     },
-    wateringDurationMinutes: {
-      value: 10
-    },
-    currentlyWatering: {
+    showMessage: {
       value: false,
-      type: "button",
-      readonly: true
-    },
-    turnPumpOn: {
-      label: "Run pump through a cycle",
-      value: false
+      label: "Show message in terminal"
     }
   }
 }, function(thing) {
-  console.log("Thing ID is", thing.id);
+  // get the thing id, and print it out
+  // console.log("Thing ID is", thing.id);
 
-  // pump object
-  var pump = {
+  // did the user set showMessage to true?
+  thing.data.pull("showMessage", function(val) {
+    if (val.value == true) {
+      // set it to false
+      thing.data.push("showMessage", false);
 
-    // is pump on?
-    pumpOn: false,
-
-    // turn on the pump
-    turnOn: function(callback) {
-      var that = this;
-      // gpio.open(config.gpiopump, "output", function(err) {
-      //   gpio.write(config.gpiopump, 1, function() {
-      //     gpio.close(config.gpiopump);
-          that.pumpOn = true;
-      //   });
-      // });
-      console.log("DO CYCLE:", true);
-    },
-
-    // turn off the pump
-    turnOff: function() {
-      var that = this;
-      // gpio.open(config.gpiopump, "output", function(err) {
-      //   gpio.write(config.gpiopump, 0, function() {
-      //     gpio.close(config.gpiopump);
-          that.pumpOn = false;
-      //   });
-      // });
-      console.log("DO CYCLE:", false);
-    },
-
-
-    // run the pump cycle
-    doCycle: function(duration, when) {
-      if (this.pumpOn) return false
-      // turn on pump
-      pump.turnOn();
-      console.log("Pump is on. Will turn off in", duration, "minutes.");
-      var countDown = duration * 60;
-
-      // count down
-      counter = function() {
-        thing.data.push("currentlyWatering", countDown, function(){});
-        countDown--;
-        if (countDown > 0) {
-          setTimeout(counter, 1000);
-        } else {
-          // turn off the pump after the cycle is complete
-          pump.turnOff();
-          when && console.log("Pump is now off. Next automatic cycle at", when, "tommorrow.") || console.log("Pump is now off.");
-          thing.data.push("currentlyWatering", false, function(){});
-        }
-      }
-      setTimeout(counter, 1000);
+      // show the message in the console
+      thing.data.pull("message", function(val) {
+        console.log("Output:", val.value);
+      });
     }
-  }
-
-  // the global timer
-  var globalTimer = setInterval(function() {
-    // test to see if it is time for the pump to turn on
-    thing.data.pull("wateringTime", function(when) {
-    // console.log(strftime('%T'), when)
-      if ( strftime('%T') == when.value ) {
-        thing.data.pull("wateringDurationMinutes", function(duration) {
-          pump.doCycle(duration.value, when.value);
-        });
-      }
-    });
-
-    // manually do a cycle
-    thing.data.pull("turnPumpOn", function(doCycle) {
-      if (doCycle.value) {
-        thing.data.push("turnPumpOn", false, function() {});
-
-        thing.data.pull("wateringDurationMinutes", function(duration) {
-          pump.doCycle(duration.value, null);
-        });
-
-      };
-    });
-
-  }, 1000);
+  });
 
 
 });
