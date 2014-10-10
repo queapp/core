a1482da8ac8eb6eaae945b76cf307ffe1666c3f7
1egoman <rsg1egoman@gmail.com>
2014-10-10 16:57:26 -0400

    updated client: auto-registering of ids

--- client/service-example/index.js
+++ client/service-example/index.js
@@ -7,13 +7,18 @@
 // the library
 var thing = require("iot-thing");
 
+// this is your app's id, set it to null and one will be auto-assigned to you
+// the id is available below as thing.id, so store it somewhere for next time and pass it in here
+id = 6
+
 // create the thing
 // specify the hostname and port of the server, the id, and
 // the structure object. This contains all the default settings for the thing.
-// new thing("que-app-backend.herokuapp.com", 80, id, {
-new thing("127.0.0.1", 8000, null, {
-  name: "Example Thing",
+new thing("que-app-backend.herokuapp.com", 80, id, {
+// new thing("127.0.0.1", 8000, id, {
+  name: "Example Service",
   desc: "Prooves that stuff works",
+  tags: ["example", "message", "terminal"],
   data: {
     message: {
       value: "Hello World"
@@ -29,15 +34,7 @@ new thing("127.0.0.1", 8000, null, {
 
   // did the user set showMessage to true?
   thing.data.pull("showMessage", function(val) {
-    if (val.value == true) {
-      // set it to false
-      thing.data.push("showMessage", false);
-
-      // show the message in the console
-      thing.data.pull("message", function(val) {
-        console.log("Output:", val.value);
-      });
-    }
+    console.log(val.value)
   });
 
 
