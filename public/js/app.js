97ffda875d2b408440bbfb112b7cea691db64e9a
1egoman <rsg1egoman@gmail.com>
2014-10-09 20:45:50 -0400

    updated frontend to support service displaying (some code porting still needed)

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-/**/
+/*/
 var host = "http://que-app-backend.herokuapp.com";
 /*/
 var host = "http://127.0.0.1:8000"
@@ -203,6 +203,150 @@ app.controller("ThingsController", function($scope, $http, thingService, $interv
 
 });
 
+app.controller("ServicesController", function($scope, $http, servicesService, $interval, $document) {
+  var root = this;
+
+  this.selectedThing = null;
+
+  // select a thing
+  this.selectThing = function(thing, event) {
+    clickable = $(event.target).hasClass("card-move");
+    cardWidth = $(".card").width();
+
+    if (event.button != 0 || !clickable) return true;
+
+    var selectThingRoot = this;
+    var move = false;
+
+    falseFunction = function(){return false;}
+    $("*").mousedown(falseFunction);
+
+    // on movement
+    mousemove = function(event) {
+      selectThingRoot.selectedThing = thing.id;
+      $('.card.above').css('left', ($(document).width()-cardWidth)/2-4 );
+      $('.card.above').css('top', event.pageY);
+      move = true;
+    };
+
+    // unbide and destruct
+    mouseup = function(event) {
+      root.selectedThing = null;
+      if (!move) {
+        return true;
+      }
+      $document.unbind('mousemove', mousemove);
+      $document.unbind('mouseup', mouseup);
+      $("*").unbind('mousedown', falseFunction);
+      $('.card.above').css('left', 0);
+      $('.card.above').css('top', 0);
+
+      // where to reorder?
+      Ry = Math.floor( event.pageY / $(".card").height() );
+
+      // do the reorder
+      root.things.splice( root.things.indexOf(thing), 1 );
+      root.things.splice(Ry, 0, thing);
+
+      // make the request
+      servicesService.reorderThing(thing.id, Ry, function() {
+
+      });
+
+      // update angular
+      $scope.$apply();
+    };
+
+    $document.on('mousemove', mousemove);
+    $document.on('mouseup', mouseup);
+  };
+
+  // get all data from server
+  servicesService.getAllThings(function(data) {
+    root.things = data;
+  });
+
+  // given a data type, get the textbox type it would go into
+  this.getTypeFor = function(value) {
+    if (value.type) {
+      return value.type;
+    } else {
+      switch (typeof value.value) {
+        case "number":
+          return "number";
+          break;
+        case "boolean":
+          return "checkbox";
+          break;
+        default:
+          return "text";
+          break;
+      }
+    }
+  }
+
+  // is this control represented as a button?
+  this.isButton = function(v) {
+    return v.type == "button";
+  }
+
+  // update backend on keypress
+  this.updateThingData = function(id, key, value, callback) {
+    data = {}
+    data[key] = value;
+
+    servicesService.updateThingData(id, data, callback || function() {});
+  }
+
+  // convert from CamelCase or underscore-format to normal, smaced words
+  this.convertIntoSpaces = function(string) {
+    string = string
+      // insert a space between lower & upper
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      // space before last upper in a sequence followed by lower
+      .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+      // uppercase the first character
+      .replace(/^./, function(str){ return str.toUpperCase(); })// insert a space between lower & upper
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      // space before last upper in a sequence followed by lower
+      .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+      // uppercase the first character
+      .replace(/^./, function(str){ return str.toUpperCase(); })
+    return string.replace('-', ' ').replace('_', ' ');
+  }
+
+  this.removeThing = function(id, index) {
+    root.things.splice(index, 1);
+    servicesService.removeThing(id, function() {});
+  }
+
+  $interval(function(){
+    servicesService.getAllThings(function(data) {
+
+      if ( angular.toJson(root.things) != angular.toJson(data) ) {
+        root.things = data;
+      }
+
+    });
+  }, 1000);
+
+});
+
+
+app.directive("thingCardList", function() {
+  return {
+    restrict: 'E',
+    templateUrl: "js/directives/thing-card-list.html"
+  }
+});
+
+app.directive("serviceCardList", function() {
+  return {
+    restrict: 'E',
+    templateUrl: "js/directives/service-card-list.html"
+  }
+});
+
 
 app.factory("thingService", function($http) {
     return {
@@ -248,6 +392,59 @@ app.factory("thingService", function($http) {
         $http({
           method: "delete",
           url: host + "/things/" + id,
+        }).success(function(data) {
+          callback(data);
+        });
+      }
+
+    };
+ });
+
+
+app.factory("servicesService", function($http) {
+    return {
+
+      getAllThings: function(callback) {
+        $http({
+          method: "get",
+          url: host + "/services/all",
+        }).success(function(data) {
+          callback(data.data);
+        });
+      },
+
+      getThingData: function(id, callback) {
+        $http({
+          method: "get",
+          url: host + "/services/" + id + "/data",
+        }).success(function(data) {
+          callback(data);
+        });
+      },
+
+      updateThingData: function(id, data, callback) {
+        $http({
+          method: "put",
+          url: host + "/services/" + id + "/data",
+          data: JSON.stringify(data)
+        }).success(function(data) {
+          callback(data);
+        });
+      },
+
+      reorderThing: function(id, newLocation, callback) {
+        $http({
+          method: "get",
+          url: host + "/services/" + id + "/location/" + newLocation,
+        }).success(function(data) {
+          callback(data);
+        });
+      },
+
+      removeThing: function(id, callback) {
+        $http({
+          method: "delete",
+          url: host + "/services/" + id,
         }).success(function(data) {
           callback(data);
         });
