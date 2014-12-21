66fcec1f8ad3069991244f038fdaf82234f85f9c
1egoman <rsg1egoman@gmail.com>
2014-12-21 16:46:36 -0500

    added thing permissions

--- routes/things.js
+++ routes/things.js
@@ -3,12 +3,13 @@
 
 // create instance of thing model
 var _ = require("underscore");
+var userCan = require("../controllers/auth").canMiddleware;
 
 // all the routes
 module.exports = function(app, things) {
 
   // get all things
-  app.get("/things/all", function(req, res, next) {
+  app.get("/things/all", userCan("thing.view.all"), function(req, res, next) {
     things.get(null, function(data) {
       res.status(200);
       res.end(JSON.stringify({data: data}));
@@ -16,7 +17,7 @@ module.exports = function(app, things) {
   });
 
   // get things that match a specific tag
-  app.get("/things/tag/:tag", function(req, res, next) {
+  app.get("/things/tag/:tag", userCan("thing.view.all"), function(req, res, next) {
     things.get(null, function(data) {
       res.send({data: _.filter(data, function(item) {
         return _.contains(item.tags, req.param("tag"));
@@ -25,7 +26,7 @@ module.exports = function(app, things) {
   });
 
   // add a new thing
-  app.post("/things/add", function(req, res, next) {
+  app.post("/things/add", userCan("thing.create"), function(req, res, next) {
     things.add(req.body, function(id) {
       if (id) {
         res.send( things.createResponsePacket("OK", {id: id}) );
@@ -36,21 +37,21 @@ module.exports = function(app, things) {
   });
 
   // list a specific thing's data
-  app.get("/things/:id/data", function(req, res, next) {
+  app.get("/things/:id/data", userCan("thing.view.#id"), function(req, res, next) {
     things.get(parseInt(req.param("id")), function(data) {
       res.send( data && data.data || things.createResponsePacket("NOHIT") );
     });
   });
 
   // list a specific thing's actions
-  app.get("/things/:id/actions", function(req, res, next) {
+  app.get("/things/:id/actions", userCan("thing.actions.view.#id"), function(req, res, next) {
     things.get(parseInt(req.param("id")), function(data) {
       res.send( data && data.actions || things.createResponsePacket("NOHIT") );
     });
   });
 
   // update thing data over http
-  app.put("/things/:id/data", function(req, res, next) {
+  app.put("/things/:id/data", userCan("thing.edit.#id"), function(req, res, next) {
     things.update(parseInt(req.param("id")), req.body, function() {
       res.send( things.createResponsePacket() );
     });
@@ -64,7 +65,7 @@ module.exports = function(app, things) {
   });
 
   // delete a thing
-  app.delete("/things/:id", function(req, res, next) {
+  app.delete("/things/:id", userCan("thing.delete.#id"), function(req, res, next) {
     things.delete(parseInt(req.param("id")), function() {
       res.send( things.createResponsePacket() );
     });
