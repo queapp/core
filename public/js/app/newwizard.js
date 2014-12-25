0e045e3cf031b2d5c9529470b3738102575327ae
1egoman <rsg1egoman@gmail.com>
2014-12-25 09:37:01 -0500

    fully implemented gui

--- public/js/app/newwizard.js
+++ public/js/app/newwizard.js
@@ -1,5 +1,6 @@
-app.controller("newWizardController", function($scope) {
+app.controller("newWizardController", function($scope, $html) {
   this.page = 0;
+  this.pageNumber = 4;
 
   this.info = {
     superAdminPassword: "",
@@ -8,7 +9,16 @@ app.controller("newWizardController", function($scope) {
     userMadePassword: ""
   }
 
-  this.nextPage = function() { this.page++; };
+  this.nextPage = function() { 
+    // go to the next page
+    this.page++; 
+
+    // on last page?
+    if (this.page+1 >= this.pageNumber) {
+      // save everything
+      this.saveAllToBackend();
+    }
+  };
   this.prevPage = function() { this.page--; };
   this.pageis = function(page) { return page === this.page };
 
@@ -28,7 +38,39 @@ app.controller("newWizardController", function($scope) {
           this.info.userMadeUsername.indexOf(' ') === -1 && 
           this.info.userMadePassword.length > 5;
         break;
+      case 3:
+        return true;
+        break;
     };
+  };
+
+  this.saveAllToBackend = function() {
+
+    // add superadmin user to backend
+    $html({
+      method: "get",
+      url: host+"/users/add",
+      body: JSON.stringify({
+        username: "superadmin",
+        pass: this.superAdminPassword
+      })
+    });
+
+    // add user created user to backend
+    $html({
+      method: "get",
+      url: host+"/users/add",
+      body: JSON.stringify({
+        username: this.info.userMadeUsername,
+        pass: this.userMadePassword
+      })
+    });
+
+    console.log("Added users.");
+
+    // no longer a new instance!
+    nC.user.newInstance = false;
+
   };
 
 });
