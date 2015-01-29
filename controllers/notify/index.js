1a38c27a1674c081473365f90dd505d00cb1859d
1egoman <rsg1egoman@gmail.com>
2015-01-29 17:54:32 -0500

    commented controller/notify

--- controllers/notify/index.js
+++ controllers/notify/index.js
@@ -1,27 +1,42 @@
-var _ = require("underscore");
+/**
+ * Notify controller. This module manages notification creation, reading, and
+ * dismissal (deleting).
+ */
 
+var _ = require("underscore");
 var Notify = require("../../models/notify");
 
 module.exports = {
   socket: null, // the websocket
 
-
+  /**
+   * Create a new notification to be shown to the user
+   * @param {string}   body  The notification textual content
+   * @param {string}   title The notification title
+   * @param {Function} done  Optional callback function - returns the newly
+   *                         created notification's id, or an error.
+   */
   createNotify: function(body, title, done) {
     var root = this;
 
+    // get notify collection contents
     Notify.find(function(err, docs) {
-      ret = [];
-      _.each(docs, function(doc) {
 
+      // was there an error?
+      if (err && done) done(err);
+
+      // convert the document list into an object list
+      ret = _.map(docs, function(doc) {
         // convert to object from model
+        // for encapsulation
         ob = doc.toObject();
         delete ob._id;
 
-        // add to array
-        ret.push(ob);
+        return ob;
       });
 
       // get max id
+      // TODO: Implementation is a bit flawed.
       maxId = _.max(ret, function(i) {
         return i.id;
       }).id || 0;
@@ -50,27 +65,42 @@ module.exports = {
     });
   },
 
-  // dismiss (delete) a notify
+  /**
+   * dismiss (delete) a notification from the collection
+   * @param {number}   id   Notification id to dismiss
+   * @param {Function} done Optional callback - returns an error if applicable.
+   */
   dismissNotify: function(id, done) {
     Notify.remove({id: id}, function(err) {
       done && done(err);
     });
   },
 
+  /**
+   * Gets all active notifications from the collection.
+   * @param  {Function} done Callback - returns an error as the first argument,
+   *                         and the object list second.
+   */
   get: function(done) {
     Notify.find(function(err, docs) {
-      ret = [];
-      _.each(docs, function(doc) {
 
-        // convert to object from model
-        ob = doc.toObject();
-        delete ob._id;
+      // was there an error?
+      if (err) done(err);
 
-        // add to array
-        ret.push(ob);
-      });
+      // convert the document list into an object list
+      // and return it through the callback
+      done(
+        null,
+        _.map(docs, function(doc) {
+          // convert to object from model
+          // for encapsulation
+          ob = doc.toObject();
+          delete ob._id;
 
-      done(err, ret);
+          return ob;
+        })
+      );
+
     });
   },
 
