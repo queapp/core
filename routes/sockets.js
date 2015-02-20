940088aeb3f9926811e030644e58c27cf080623a
1egoman <rsg1egoman@gmail.com>
2015-02-20 07:42:21 -0500

    socket.io permissions - fixed a big security flaw

--- routes/sockets.js
+++ routes/sockets.js
@@ -1,6 +1,7 @@
 // set up socket.io
 var http = require('http');
 var socket = require('socket.io');
+var userCan = require("../controllers/auth").can;
 
 module.exports = function(app, http, things, rooms) {
 
@@ -14,21 +15,35 @@ module.exports = function(app, http, things, rooms) {
     // (things) frontend -> backend
     socket.on('push-thing-data-update', function(changed) {
 
-      // update the backend
-      things.update(changed.id, changed.data);
+      // is the user allowed to edit a thing?
+      userCan(changed.auth, "thing.edit.#id", function(err, allowed) {
+        if (allowed) {
 
-      // propagate the changes (backend -> frontend)
-      io.emit('pull-thing-data-update', changed);
+          // update the backend
+          things.update(changed.id, changed.data);
+
+          // propagate the changes (backend -> frontend)
+          io.emit('pull-thing-data-update', changed);
+        };
+      });
+
     });
 
     // (rooms) frontend -> backend
     socket.on('push-room-data-update', function(changed) {
 
-      // update the backend
-      room.update(changed.id, changed.data);
+      // is the user allowed to edit a room?
+      userCan(changed.auth, "room.edit.#id", function(err, allowed) {
+        if (allowed) {
 
-      // propagate the changes (backend -> frontend)
-      io.emit('pull-service-data-update', changed);
+          // update the backend
+          rooms.update(changed.id, changed.data);
+
+          // propagate the changes (backend -> frontend)
+          io.emit('pull-room-data-update', changed);
+        };
+      });
+
     });
 
     // the client diconnected
