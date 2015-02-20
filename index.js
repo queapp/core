ca7bc0b550341fa3d2edd30ab1a99fc0275f31a7
1egoman <rsg1egoman@gmail.com>
2015-02-20 08:37:30 -0500

    initial cluster support

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var http = require("http");
 var passport = require('passport');
 var cors = require('cors');
+var cluster = require('express-cluster');
+var chalk = require('chalk');
 
 // express middleware
 var bodyParser = require("body-parser");
@@ -24,32 +26,46 @@ if (process.env.MONGOURI || argv.db) {
 require("./controllers/logger")(argv, function(logger) {
   var routes = require("./routes");
 
-  var app = express();
+  var go = function(worker) {
+    var app = express();
 
-  // some middleware
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use(session({secret: "secret", saveUninitialized: true, resave: true}));
-  app.use(less(__dirname+'/public'))
+    // some middleware
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(session({secret: "secret", saveUninitialized: true, resave: true}));
+    app.use(less(__dirname+'/public'))
 
-  // static hosting of /public
-  app.use(express.static(__dirname+'/public'));
+    // static hosting of /public
+    app.use(express.static(__dirname+'/public'));
 
-  // log all http requests
-  app.use( require('winston-request-logger').create(logger) );
+    // log all http requests
+    app.use( require('winston-request-logger').create(logger) );
 
-  // create http Server
-  var server = http.Server(app);
+    // create http Server
+    var server = http.Server(app);
 
-  // create all of the routes
-  var io = routes(app, server, argv);
+    // create all of the routes
+    var io = routes(app, server, argv, worker);
 
-  // run server
-  server.listen(process.env.PORT || 8000, function() {
+    // run server
+    return server.listen(process.env.PORT || 8000, function() {
 
-    // log the port
-    console.log("Listening on port %d", server.address().port)
+      // log the port
+      console.log("%s Listening on port %d", chalk.yellow("[w"+worker.id+"]"), server.address().port)
 
-  });
+    });
+  };
+
+  // production?
+  var workerCount;
+  if (process.argv.NODE_ENV === "production") {
+    // Count the machine's CPUs
+    workerCount = require('os').cpus().length;
+  } else {
+    workerCount = 1;
+  }
+
+  // create workers
+  cluster(go, {count: workerCount, verbose: true})
 
 });
